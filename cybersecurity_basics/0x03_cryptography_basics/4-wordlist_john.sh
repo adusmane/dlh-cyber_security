9b16fe3004f7347b7578d53845082df024f9435d
adusmane <dusalex27@yahoo.com>
2026-05-29 14:44:45 +0000

    again

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt --format=raw-md5 "$1" --stdout | grep -Ff - hash.txt > 4-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt --format=raw-md5 "$1" --stdout > wordlist.txt && hashcat -m 0 -a 0 "$1" wordlist.txt -o 4-password.txt
