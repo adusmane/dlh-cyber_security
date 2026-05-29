6904b83358421108ec6e3fb86458426158946ea0
adusmane <dusalex27@yahoo.com>
2026-05-29 15:03:57 +0000

    changed

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt --format=raw-md5 "$1" && john --show "$1" | tail -n +2 | cut -d: -f2 > 4-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt --format=raw-md5 "$1" --stdout | grep -Ff - "$1" > 4-password.txt
