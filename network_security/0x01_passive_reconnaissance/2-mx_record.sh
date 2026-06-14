249870fd5fdaaa3bd6f4141ce27496425da2034d
adusmane <dusalex27@yahoo.com>
2026-06-14 18:08:24 +0000

    3try 2

--- network_security/0x01_passive_reconnaissance/2-mx_record.sh
+++ network_security/0x01_passive_reconnaissance/2-mx_record.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nslookup -type=MX "$1" 2>/dev/null | grep -E "^(Server:|Address:|$1.*mail exchanger)"
+nslookup -type=MX "$1" 2>/dev/null | grep -E "^(Server:|Address:|.*mail exchanger =)"
