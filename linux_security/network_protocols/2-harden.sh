3b92679d43a0b4ce595daaa02d800c1746e8db8b
adusmane <dusalex27@yahoo.com>
2026-06-14 16:28:07 +0000

    tryagain

--- linux_security/network_protocols/2-harden.sh
+++ linux_security/network_protocols/2-harden.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find / -type d -perm -o+w 2>/dev/null | while read dir; do echo "$dir"; chmod 755 "$dir"; done
+find / -type d -perm -o+w 2>/dev/null | while read dir; do chmod 755 "$dir" && printf "%s\n" "$dir"; done
