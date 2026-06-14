43144a39005c5742ce169aeaff16a90b4372246a
adusmane <dusalex27@yahoo.com>
2026-06-14 17:16:30 +0000

    smtp8

---
+++ linux_security/network_protocols/8-smtp.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+grep -E 'smtpd_tls_security_level|smtpd_use_tls|smtp_tls_security_level|smtpd_tls_mandatory_protocols|smtpd_tls_mandatory_ciphers' /etc/postfix/main.cf 2>/dev/null | grep -v '^#' || echo "STARTTLS not configured"
