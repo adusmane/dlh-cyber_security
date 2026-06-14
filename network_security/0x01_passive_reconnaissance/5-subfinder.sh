a7833132d24a2c039fe56350dd126a68797db7b6
adusmane <dusalex27@yahoo.com>
2026-06-14 19:04:05 +0000

    again

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-subfinder -d $1 -silent -oI -o $1.txt | cut -d',' -f1
+subfinder -d $1 -silent | while read domain; do echo "$domain,$(dig +short $domain | head -1)"; done | grep -v ",$" | tee $1.txt
